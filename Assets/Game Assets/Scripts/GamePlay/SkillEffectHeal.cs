3127066f190c6a08cd52d6c8c473e9ccd0be120c
nakarin <nakarin@iconext.co.th>
2016-01-25 10:58:52 +0700

    Update skill

---
+++ Assets/Game Assets/Scripts/GamePlay/SkillEffectHeal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetdShooting.GamePlay
+{
+    public class SkillEffectHeal : SkillEffectCalculator
+    {
+        public SkillEffectHeal(SkillEffect effect)
+            : base(effect)
+        {
+
+        }
+
+        public override void Apply(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float GetBeforeApplyEffectValue(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float GetEffectValue(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Unapply(Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
